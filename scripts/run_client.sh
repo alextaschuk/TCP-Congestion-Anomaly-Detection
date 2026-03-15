f15d77f28fb7af0ee6c71a9ccc0380df476c33a8
alextaschuk <alextaschuk@gmail.com>
2026-03-15 11:42:02 -0700

    fixed hashing command

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -5,7 +5,8 @@
 rm ../test_rcvd.txt && touch ../test_rcvd.txt
 
 bash ../scripts/reset_logs.sh
+echo "Reset logs"
 
 time ./client_app
 
-sha384 ../test_rcvd.txt
+sha384sum ../test_rcvd.txt
--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,10 +3,12 @@
 # script should be run from /build directory via `bash ../scripts/run_server.sh`
 
 # generate new 1GB test file
-rm test_file.txt && cat /dev/urandom | base64 | head -c 1000000000 > test_file.txt 
+rm test_file.txt && cat /dev/urandom | base64 | head -c 1000000000 > test_file.txt
+echo "Regenerated 1GB test file"
 
 bash ../scripts/reset_logs.sh
+echo "Reset logs"
 
 time ./server_app
 
-sha384 ../test_file.txt
+sha384sum ../test_file.txt
