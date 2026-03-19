d1304e903f839f652a4dc65f70ac8b00cbb2f98e
alextaschuk <alextaschuk@gmail.com>
2026-03-18 18:21:58 -0700

    Bug: swap to send data client -> server

--- src/utcp/server.c
+++ src/utcp/server.c
@@ -36,48 +36,64 @@ _Thread_local const char* current_thread_cat = "main_thread";
 
 int main(void)
 {
-        if (init_zlog("zlog_client.conf") != 0) // initialize logger
+    if (init_zlog("zlog_server.conf") != 0) // initialize logger
         err_sys("Error initializing zlog");
 
-    bind_udp_sock(0);
+    api_t *global = api_instance();
+    global->utcp_fd = 0; // listen socket will always have fd of 0
     int utcp_fd = utcp_sock();
 
-    struct sockaddr_in client =
-    {
+    struct sockaddr_in server = {
         .sin_family = AF_INET, 
-        .sin_port = htons(8292), 
+        .sin_port = htons(332), // UTCP port 
         .sin_addr.s_addr = htonl(INADDR_ANY)
     };
+    
+    utcp_bind(utcp_fd, &server);
+    log_tcb(get_tcb(utcp_fd), "Post init TCB:");
 
-    struct sockaddr_in server =
-    {
-        .sin_family = AF_INET, 
-        .sin_port = htons(332), 
-        .sin_addr.s_addr = inet_addr("40.82.162.155")
-    };
+    if (utcp_listen(global, MAX_BACKLOG) != 0)
+        err_sys("[main] Error in utcp_listen");
 
-    utcp_bind(utcp_fd, &client);
-    utcp_connect(utcp_fd, &server);
+    if(spawn_threads(global) != 0)
+        err_sys("[main] Error during thread creation");
+    
+    tcb_t *listen_tcb = get_tcb(utcp_fd);
 
-    FILE *fp = fopen("../test_rcvd.txt", "wb"); // wb to ensure it's an exact copy
+    pthread_mutex_lock(&listen_tcb->lock);
+    listen_tcb->src_udp_fd = global->udp_fd;
+    pthread_mutex_unlock(&listen_tcb->lock);
+
+    int new_utcp_fd = utcp_accept(global);
+    tcb_t *new_tcb = get_tcb(new_utcp_fd);
+
+    pthread_mutex_lock(&new_tcb->lock);
+    new_tcb->src_udp_fd = global->udp_fd;
+    pthread_mutex_unlock(&new_tcb->lock);
+
+    FILE *fp = fopen("../test_file.txt", "rb"); // rb to prevent OS from changing newline characters
     if (!fp)
-        err_sys("[Client App] Failed to create destination file");
-
+        err_sys("[Server App] Failed to open text file");
+            
+    struct stat st;
+    if (stat("../test_file.txt", &st) == -1)
+        err_sys("[Client App] Failed to get filesize");
+    
     size_t file_size_bytes = 1000000000;// 1GB
-    //uint8_t *app_rcv_buf = malloc(APP_BUF_SIZE);
-    uint8_t *app_rcv_buf = malloc(65536);
-    size_t total_received = 0;
+    //uint8_t *app_recv_buf = malloc(APP_BUF_SIZE);
+    uint8_t *app_recv_buf = malloc(65536);
+    size_t total_recvd = 0;
 
     printf("Client: Ready to receive %zuGB file from server...\r\n", file_size_bytes / 1000000000);
-    while(total_received < file_size_bytes)
+    while(total_recvd < file_size_bytes)
     {   
-        ssize_t bytes_rcvd = utcp_recv(utcp_fd, app_rcv_buf, APP_BUF_SIZE);
+        ssize_t bytes_rcvd = utcp_recv(utcp_fd, app_recv_buf, APP_BUF_SIZE);
         if (bytes_rcvd > 0)
         {
-            fwrite(app_rcv_buf, 1, bytes_rcvd, fp);
+            fwrite(app_recv_buf, 1, bytes_rcvd, fp);
             fflush(fp); // forces the OS to write to the txt file immediately
-            total_received += (size_t)bytes_rcvd;
-            printf("Client Application: Wrote %zd bytes to disk. Total: %zu/%zu\r", bytes_rcvd, total_received, file_size_bytes);
+            total_recvd += (size_t)bytes_rcvd;
+            printf("Client Application: Wrote %zd bytes to disk. Total: %zu/%zu\r", bytes_rcvd, total_recvd, file_size_bytes);
             fflush(stdout);
         }
         if (bytes_rcvd < 0)
@@ -91,10 +107,11 @@ int main(void)
         usleep(100000); // let everything in RX go to the client
     sleep(2);
 
-    LOG_INFO("[Client App] Finished. Received %zu bytes total", total_received);
+    LOG_INFO("[Client App] Finished. Received %zu bytes total", total_recvd);
     fclose(fp);
-    free(app_rcv_buf);
+    free(app_recv_buf);
     return 0;
+
     /*
     if (init_zlog("zlog_server.conf") != 0) // initialize logger
         err_sys("Error initializing zlog");
